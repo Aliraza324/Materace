0351a2588a7e254ed2cdb1073af840e72613dd5c
aliraza324 <alirazavn2342@gmail.com>
2026-08-19 17:40:43 +0500

    ++

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import React, { useState } from "react";
 import Home from "./pages/home/Home";
+import Loader from "./components/common/Loader";
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
-      <Home />
-    
+      <Loader onLoadingComplete={() => setIsLoading(false)} />
+      <div className={`transition-opacity duration-700 ${isLoading ? "opacity-90 scale-[0.99]" : "opacity-100 scale-100"}`}>
+        <Home />
+      </div>
     </>
   );
 };
+
 export default App;
